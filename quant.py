ecebc8898cfe221d970d9959dc7e8445a188497c
q3world <q3world@sina.com>
2021-11-09 20:55:22 +0800

    Add files via upload

--- quant.py
+++ quant.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import datetime
 import requests
@@ -14,8 +15,11 @@ import threading
 import cn_stock_holidays.data as ccal
 
 import akshare
+from pytdx.hq import TdxHq_API
+from pytdx.config.hosts import hq_hosts
 
 class quote_base:
+    _decl = None
     _resample_rules = {
             '1m':   '1T',
             '2m':   '2T',
@@ -41,6 +45,8 @@ class quote_base:
             '2y':   '2Y',
             '3y':   '3Y'
             }
+
+    _session = None
     
     def __init__(self):
         pass
@@ -48,12 +54,17 @@ class quote_base:
     def sleep(self):
         pass
     
+    def get_decl(self):
+        return self._decl
+    
+    def get_first(self):
+        return next(iter(self._decl))
+    
     def get_resample_rules(self):
         return self._resample_rules
     
     def get_symbol(self, symbol):
         vs = symbol.split('.')
-        
         return vs[1], vs[0]
         
     def during(self):
@@ -111,11 +122,13 @@ class quote_china(quote_base):
                 df.index += (1 - bins) * pd.Timedelta('30 min')
             elif rule == '2H':
                 rule = '4H'
+
+            #print(df, interval)
             
             ohlcv = { 'open': 'first', 'close': 'last', 'high': 'max', 'low': 'min',  'volume': 'sum' }
             return df.resample(rule, closed='right', label='right').agg(ohlcv).dropna()
 
-class quote_Ak(quote_china):
+class quote_AkShare(quote_china):
     _real = { '1m': 1, '5m': 5, '15m': 15, '30m': 30, '1h': 60, '1d': 'daily', '1w': 'weekly', '1M': 'monthly' }
     _decl = {
             '5m':   [None, None],
@@ -129,15 +142,17 @@ class quote_Ak(quote_china):
             '1M':   [None, '1d'],
             }
     
-    def __init__(self):
-        pass
+    def get_symbols(self):
+        df = akshare.stock_zh_a_spot_em()
+        
+        vs = {}
+        for row in df.itertuples():
+            _symbol = row[2]
+            _market = self.get_market(_symbol)
+            vs[f'{_symbol}.{_market}'] = row[3]
 
-    def get_decl(self):
-        return self._decl
-    
-    def get_first(self):
-        return next(iter(self._decl))
-    
+        return vs
+        
     def get_candle(self, symbol, interval=None, flag=0):
         if interval:
             if interval in self._real:
@@ -150,27 +165,83 @@ class quote_Ak(quote_china):
                     df = akshare.stock_zh_a_hist_min_em(symbol=_symbol, period=_interval, adjust='qfq')
                     df.rename(columns={'时间': 'time', '开盘': 'open', '最高': 'high', '最低': 'low', '收盘': 'close', '成交量': 'volume'}, inplace=True)
                     
-                #print(df)
                 df.drop(columns=['涨跌幅', '涨跌额', '成交额', '振幅', '换手率'], inplace=True)
                 df.time = pd.to_datetime(df.time)
                 df.set_index(['time'], inplace=True)
-                #print(df)
                 
                 return df
             
         else:
             return self.get_candle(symbol, self.get_first(), 1)
             
-    def get_symbols(self):
-        df = akshare.stock_zh_a_spot_em()
-        
-        root = {}
-        for row in df.itertuples():
-            _symbol = row[2]
-            _market = self.get_market(_symbol)
-            root[f'{_symbol}.{_market}'] = row[3]
+class quote_PyTdx(quote_china):
+    _real = { '1m': 8, '5m': 0, '15m': 1, '30m': 2, '1h': 3, '1d': 4, '1w': 5, '1M': 6, '1q': 10, '1y': 11 }
+    _decl = {
+            '5m':   [None, None],
+            '10m':  ['5m', '5m'],
+            '15m':  [None, '5m'],
+            '30m':  [None, '15m'],
+            '1h':   [None, '30m'],
+            '2h':   ['1h', '1h'],
+            '1d':   [None, '1h'],
+            '1w':   [None, '1d'],
+            '1M':   [None, '1d'],
+            }
+    
+    def get_session(self):
+        if not self._session:
+            self._session = TdxHq_API(heartbeat=True)
+            if not self._session.connect('119.147.212.81', 7709):
+                return
+            
+        return self._session
 
-        return root
+    def get_symbols(self):
+        _session = self.get_session()
+        if _session:
+            vs = {}
+            for _market in (0, 1):
+                _count = 0
+                while 1:
+                    try:
+                        data = _session.get_security_list(_market, _count)
+                        if not data:
+                            break
+
+                        for v in data:
+                            _symbol = v['code']
+                            vs[f'{_symbol}.{_market}'] = v['name']
+
+                        _count += len(data)
+                        
+                    except Exception as e:
+                        pass
+                        
+            return vs
+        
+    def get_symbol(self, symbol):
+        vs = symbol.split('.')
+        return 0 if vs[1] == '0' else 1, vs[0]
+        
+    def get_candle(self, symbol, interval=None, flag=0):
+        _session = self.get_session()
+        if _session:
+            if interval in self._real:
+                _market, _symbol = self.get_symbol(symbol)
+                _interval = self._real[interval]
+                _count = 100 if flag else 800
+                try:
+                    data = _session.get_security_bars(_interval, _market, _symbol, 0, _count)
+                    df = _session.to_df( data )
+                    df.drop(columns=['year', 'month', 'day', 'hour', 'minute', 'amount'], inplace=True)
+                    df.rename(columns={'datetime': 'time', 'vol': 'volume'}, inplace=True)
+                    df.time = pd.to_datetime(df.time)
+                    df.set_index(['time'], inplace=True)
+                    
+                    return df
+                
+                except Exception as e:
+                    pass
         
 class strategy_base:
     pass
@@ -178,27 +249,49 @@ class strategy_base:
 class strategy_MA(strategy_base):
     def execute(self, symbol, cache):
         def df_compute(symbol, interval, df):
+            c_values = df['close'].values
+            price = c_values[-1]
+            l_values = df['low'].values
+            
             vs = []
             time_period_list = [5, 10, 20, 30, 60, 120, 240]
             for time_period in time_period_list:
                 if time_period < len(df):
-                    c_values = df['close'].values
-                    l_values = df['low'].values
                     ma = ta.MA(c_values, timeperiod=time_period, matype=0)
-                    #print(ma)
-                
-                    price = c_values[-1]
                     value = ma[-1]
-                    #print('{:>10} {:>10.3f} {:>10} {:>10d} {:>10.4f} {:>10.4%}'.format(symbol, price, interval, time_period, value, price/value-1) )
                     if price > value and price < value*1.01 and ma[-1] > ma[-2] and ma[-2] > ma[-3]:
                         flag = 0
-                        for i in range(time_period//2 + 1):
+                        for i in range(time_period//3 + 1):
                             if l_values[-i] > ma[-i]:
                                 flag = 1
                                 break
 
                         if not flag:
                             vs.append({'symbol': symbol, 'interval': interval, 'function': f'ma{time_period}', 'price': price, 'value': np.around(value, digits)})
+
+            return vs
+
+        digits = market.get_quote(symbol).get_digits(symbol)
+        
+        vs = []
+        for i in cache:
+            v = df_compute(symbol, i, cache[i])
+            if v:
+                vs.extend(v)
+        
+        return vs
+    
+class strategy_MACD(strategy_base):
+    def execute(self, symbol, cache):
+        def df_compute(symbol, interval, df):
+            param = (12, 26, 9)
+            
+            c_values = df['close'].values
+            price = c_values[-1]
+            
+            vs = []
+            macd = ta.MACD(c_values, fastperiod=param[0], slowperiod=param[1], signalperiod=param[2])
+            print(macd)
 
             return vs
 
@@ -254,10 +347,12 @@ class market:
             time.sleep(v)
     
     def get_quote(symbol):
-        if not 'Ak'in market._quotes:
-            market._quotes['Ak'] = quote_Ak()
+        v = 'AkShare'
+        v = 'PyTdx'
+        if not v in market._quotes:
+            market._quotes[v] = getattr(sys.modules[__name__], f'quote_{v}')()
         
-        return market._quotes['Ak']
+        return market._quotes[v]
     
     def get_symbols(symbol):
         if not symbol in market._symbols:
@@ -325,12 +420,17 @@ class market:
         #print(cache)
         
     def get_strategy():
-        if not 'MA'in market._strategies:
-            market._strategies['MA'] = strategy_MA()
+        vs = ['MA', 'MACD']
+        vs = ['MA']
+        for v in vs:
+            if not v in market._strategies:
+                market._strategies[v] = getattr(sys.modules[__name__], f'strategy_{v}')()
         
         return market._strategies
         
     def run_single(cache, force=0):
+        print(pd.Timestamp.now())
+
         result = False
         
         for symbol in market._selection:
@@ -361,19 +461,23 @@ class market:
                                 print('{} {:<5} {:>10.3f} {:>3} {:<5} {:>8.3f} {:>8.3f} {:>8.3f}%'.format(v['symbol'], title, v['price'], v['interval'], v['function'], v['value'], diff, pt))
                             
                             winsound.PlaySound('*', winsound.SND_ALIAS)
-                            
+
             result = result or flag
 
-        #print(cache)
-            
         return result
 
+    def write_cache(cache):
+        for symbol in cache:
+            for interval in cache[symbol]:
+                cache[symbol][interval].to_csv(f'r:/{symbol}.{interval}.txt')
+                
     def run_target():
         print('Start...')
         market.load_config()
         cache = {}
         
         market.run_single(cache, 1)
+        #market.write_cache(cache)
         
         while market._running:
             full_during = market.run_single(cache)
@@ -388,5 +492,8 @@ class market:
             threading.Thread(target=market.run_target).start()
             
 if __name__ == '__main__':
+    #print(quote_PyTdx().get_symbols())
+    #print(quote_PyTdx().get_candle('000001.0', '1d'))
+    
     market.run_target()
     #market.run()
